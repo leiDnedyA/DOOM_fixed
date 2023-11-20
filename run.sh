b4af913af9213c9287cff6c43e63b7af32096b6d
leiDnedyA <aydendiel@gmail.com>
2023-11-20 13:30:33 -0500

    Created shell script to build and run game

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+source ~/.bashrc
+
+gnome-terminal -e 'Xephyr :2 -ac -screen 320x200x8'
+
+cd linuxdoom-1.10/
+
+make
+
+cd linux/
+
+curl -O https://distro.ibiblio.org/slitaz/sources/packages/d/doom1.wad # download the shareware wad
+
+DISPLAY=:2
+
+./linuxxdoom
